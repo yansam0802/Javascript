bf049d0a4e47151637a17adc4ee91e0450d6d0f8
yansam0802 <114323258+yansam0802@users.noreply.github.com>
2023-06-22 22:58:32 -0500

    ejercicio.js

---
+++ ejercicio.js
@@ -0,0 +1,19 @@
+/*
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+*/
+const lista = ["Yan", 28, true]
+
+//- Tu fecha de nacimiento (Date)
+const fecha_nacimiento = new Date(1955, 4, 19);
+console.log(fecha_nacimiento);
+
+
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+const librofavorito = {
+    titulo: "cien años de soledad",
+    autor: "Gabriel Garcia Marquez",
+    fecha: 1995,
+    url: "https//www.cienañosdesoledad.com"
+}
