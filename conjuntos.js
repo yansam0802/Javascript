85b8e3db31d1798711628b8f157dafa2525c19ab
yansam0802 <114323258+yansam0802@users.noreply.github.com>
2023-06-26 21:16:36 -0500

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,12 @@
+//- Un nuevo Set con los nombres de tu familia
+
+const listaFamilia = ["Samuel", "Danna", "Sara", "Yan"]
+
+const miFamilia1 = new Set(listaFamilia)
+console.log(listaFamilia)
+
+miFamilia1.add("Yan")
+console.log(miFamilia1)
+
+miFamilia1.add("Javascript")
+console.log(miFamilia1)
